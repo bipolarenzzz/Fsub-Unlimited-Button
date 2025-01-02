a7f2c7a1d6d8c0b2f5367e434b75b7c2c42318a5
bipolarenzzz <134811612+bipolarenzzz@users.noreply.github.com>
2025-01-03 04:22:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import os
-from pyrogram import Client
+from bot import Bot
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name="my_bot",
-            api_id=int(os.environ["APP_ID"]),
-            api_hash=os.environ["API_HASH"],
-            bot_token=os.environ["TG_BOT_TOKEN"],
-        )
-
-if __name__ == "__main__":
-    # Langsung jalankan bot
-    Bot().run()
+Bot().run()
