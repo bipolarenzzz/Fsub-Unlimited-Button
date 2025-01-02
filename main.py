f068944b6ce900c851c79a8117e84c57059c90fb
bipolarenzzz <134811612+bipolarenzzz@users.noreply.github.com>
2025-01-03 04:16:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import os
-from asyncio import run
 from pyrogram import Client
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
             name="my_bot",
-            # Dibaca dari environment variable Heroku
             api_id=int(os.environ["APP_ID"]),
             api_hash=os.environ["API_HASH"],
             bot_token=os.environ["TG_BOT_TOKEN"],
         )
 
 if __name__ == "__main__":
-    run(Bot().start())
+    # Langsung jalankan bot
+    Bot().run()
